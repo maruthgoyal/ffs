cbfdc2a825acd3ccef403c82f69df1d37bef02f2
maruthgoyal <maruthgoyal@gmail.com>
2016-06-07 12:05:55 -0400

    Added some doc

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+
+/* Takes command to execute as input. Executes the command in the terminal and returns the output */
 string exec(const char* cmd) {
 
     char buffer[128];
@@ -25,6 +27,7 @@ string exec(const char* cmd) {
     return result;
 }
 
+/* Split a string with the char c as the denominator  */
 vector<string> split(const char *str, char c = ' '){
     
     vector<string> result;
@@ -50,13 +53,13 @@ int main(){
     unordered_map<string, int> freqs; 
     vector< pair<int, string> > f;
 
-    last_command = exec("bash -c \"sudo cat ~/.bash_history\" | awk -v n=2 '{x[NR%n]=$0}END{print x[(NR+1)%n]}'");
+    last_command = exec("bash -c \"sudo cat ~/.bash_history\" | awk -v n=2 '{x[NR%n]=$0}END{print x[(NR+1)%n]}'"); // Get last command executed
 
 
-    last_command.erase(remove(last_command.begin(), last_command.end(), '|'), last_command.end());
+    last_command.erase(remove(last_command.begin(), last_command.end(), '|'), last_command.end()); // remove pipe symbol. We want to remove other symbols too, like ; > < etc. to create better tokens
     
     
-    tokens = split(last_command.c_str());
+    tokens = split(last_command.c_str()); // create the tokens
     int tokensSize = tokens.size();
 
     for(int i  = 0; i < tokensSize; i++){
@@ -68,6 +71,9 @@ int main(){
             for(int j = 0; j < (len - 1); j++){
                 
                 list = split((exec(("bash -c \"compgen -ac | grep -o -w '\\w\\{" + to_string(len) + "\\}' | grep '" + tokens[i][j] + tokens[i][j+1] + "' | sort -u\"").c_str()).c_str()), '\n');
+
+                // Get list of all commands. Take those of the same length as the command entered. Match every length-2 subsequence of the users input and filter out copies. 
+                // Generates list of possible matches
                
                 results.reserve(results.size() + distance(list.begin(), list.end()));
                 results.insert(results.end(), list.begin(), list.end());
